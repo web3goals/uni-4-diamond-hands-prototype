e7ec1e307133d6e8558c8df7d34982b1201769b2
kiv1n <aloha@kiv1n.ru>
2025-05-17 10:24:18 +0300

    Add site configuration, global styles, and favicon

---
+++ app/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "UNI 4 Diamond Hands",
+  description:
+    "A UNI-powered platform to create quizzes that turn your project's token holders into diamond-handed supporters",
+  links: {
+    github: "https://github.com/web3goals/uni-4-diamond-hands-prototype",
+    twitter: "https://twitter.com/kiv1n",
+  },
+};
